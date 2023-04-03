9e1bd3623216d49ddcaa5fb6a7914eee9468e214
IdrisFallout <waithakasam2017@gmail.com>
2023-04-03 20:12:43 +0300

    setup alarm panel

--- IOTAlarmApp/Assets/Scripts/SetupAlarmPanel.cs
+++ IOTAlarmApp/Assets/Scripts/SetupAlarmPanel.cs
@@ -1,13 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class SetupAlarmPanel : MonoBehaviour
 {
+    [SerializeField]
+    private TextMeshProUGUI TimeText;
+    
+    [SerializeField]
+    private TextMeshProUGUI AmPmText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        DateTime time = System.DateTime.Now;
+        TimeText.text = time.ToString("hh:mm");
+        AmPmText.text = time.ToString("tt");
     }
 
     // Update is called once per frame
