643c65225f6ebc660b041d9439d4639a178937b8
IdrisFallout <waithakasam2017@gmail.com>
2023-04-08 16:45:51 +0300

    api request

--- IOTAlarmApp/Assets/Scripts/AlarmActivity.cs
+++ IOTAlarmApp/Assets/Scripts/AlarmActivity.cs
@@ -3,72 +3,68 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using System.Threading;
 using UnityEngine.Networking;
+using Newtonsoft.Json;
 
 public class AlarmActivity : MonoBehaviour
 {
     [SerializeField]
     private TextMeshProUGUI textMeshPro;
-    
-    private Thread syncThread;
-    private bool isSyncing = false;
-    System.Diagnostics.Stopwatch stopwatch;
 
     private void Update()
     {
         textMeshPro.text = System.DateTime.Now.ToString("hh:mm:ss");
-        CheckSyncStatus();
     }
 
-    
-    // run this method in a separate thread
-    public void SyncToCloud()
+    IEnumerator GetResponse(string url)
     {
-        Debug.Log("Syncing to cloud");
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(url))
+        {
+            yield return webRequest.SendWebRequest();
+
+            if (webRequest.result == UnityWebRequest.Result.Success)
+            {
+                // Read the response content as text
+                string responseBody = webRequest.downloadHandler.text;
+                Debug.Log("Response: " + responseBody);
+            }
+            else
+            {
+                Debug.Log("Error: " + webRequest.error);
+            }
+        }
     }
 
     public void SendToCloud()
     {
-        if (!isSyncing)
-        {
-            StartSyncThread();
-        }
+        Debug.Log(PackageAlarmJson()["time"]);
+        // string url = "https://jsonplaceholder.typicode.com/todos/1";
+        // StartCoroutine(GetResponse(url));
+    }
+    public class MyAlarm
+    {
+        public int Number { get; set; }
+        public string Time { get; set; }
+        public bool State { get; set; }
     }
     
-    private void StartSyncThread()
+    public static Dictionary<string, object> PackageAlarmJson()
     {
-        // Create and start a new thread for the SyncToCloud function
-        syncThread = new Thread(SyncToCloud);
-        syncThread.Start();
-        stopwatch = new System.Diagnostics.Stopwatch();
-        stopwatch.Start();
-        isSyncing = true;
-    }
+        MyAlarm myAlarm = new MyAlarm
+        {
+            Number = 1,
+            Time = "12:00 AM",
+            State = true
+        };
+        
+        Dictionary<string, object> jsonAlarmObject = new Dictionary<string, object>
+        {
+            { "number", myAlarm.Number },
+            { "time", myAlarm.Time },
+            { "state", myAlarm.State }
+        };
 
-    private void OnDestroy()
-    {
-        StopSyncThread();
+        return jsonAlarmObject;
     }
     
-    private void StopSyncThread()
-    {
-        // Stop the syncThread if it's running
-        if (syncThread != null && syncThread.IsAlive)
-        {
-            syncThread.Abort();
-        }
-    }
-
-    public void CheckSyncStatus()
-    {
-        // Check if the thread is still running
-        if (isSyncing && syncThread != null && !syncThread.IsAlive)
-        {
-            stopwatch.Stop();
-            // Thread has finished, perform any necessary operations here
-            Debug.Log("Syncing to cloud thread has finished. It took " + stopwatch.ElapsedMilliseconds + " milliseconds");
-            isSyncing = false;
-        }
-    }
 }
