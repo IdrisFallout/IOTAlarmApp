0534fc658c6ecf9e30873186c838a7132be9cc97
IdrisFallout <waithakasam2017@gmail.com>
2023-04-08 14:06:29 +0300

    sync alarm to cloud

--- IOTAlarmApp/Assets/Scripts/AlarmActivity.cs
+++ IOTAlarmApp/Assets/Scripts/AlarmActivity.cs
@@ -3,14 +3,70 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using System.Threading;
 
 public class AlarmActivity : MonoBehaviour
 {
     [SerializeField]
     private TextMeshProUGUI textMeshPro;
+    
+    private Thread syncThread;
+    private bool isSyncing = false;
+    System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
 
     private void Update()
     {
         textMeshPro.text = System.DateTime.Now.ToString("hh:mm:ss");
+        CheckSyncStatus();
+    }
+
+    
+    // run this method in a separate thread
+    public void SyncToCloud()
+    {
+        stopwatch.Start();
+        Debug.Log("Syncing to cloud");
+    }
+
+    public void SendToCloud()
+    {
+        if (!isSyncing)
+        {
+            StartSyncThread();
+        }
+    }
+    
+    private void StartSyncThread()
+    {
+        // Create and start a new thread for the SyncToCloud function
+        syncThread = new Thread(SyncToCloud);
+        syncThread.Start();
+        isSyncing = true;
+    }
+
+    private void OnDestroy()
+    {
+        StopSyncThread();
+    }
+    
+    private void StopSyncThread()
+    {
+        // Stop the syncThread if it's running
+        if (syncThread != null && syncThread.IsAlive)
+        {
+            syncThread.Abort();
+        }
+    }
+
+    public void CheckSyncStatus()
+    {
+        // Check if the thread is still running
+        if (isSyncing && syncThread != null && !syncThread.IsAlive)
+        {
+            stopwatch.Stop();
+            // Thread has finished, perform any necessary operations here
+            Debug.Log("Syncing to cloud thread has finished. It took " + stopwatch.ElapsedMilliseconds + " milliseconds");
+            isSyncing = false;
+        }
     }
 }
